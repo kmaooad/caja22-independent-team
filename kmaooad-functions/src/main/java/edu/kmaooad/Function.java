6963c5320a07d329e74bea8c171ebf564cc52b0e
ivanmaister7 <ivanmaister7@gmail.com>
2022-10-20 16:15:59 +0300

    Add some tests for coverage code.

--- kmaooad-functions/src/main/java/edu/kmaooad/Function.java
+++ kmaooad-functions/src/main/java/edu/kmaooad/Function.java
@@ -25,6 +25,7 @@ public class Function {
      * This function listens at endpoint "/api/TelegramWebhook". To invoke it using "curl" command in bash:
      * curl -d "HTTP Body" {your host}/api/TelegramWebhook
      */
+    private static final String BAD_RESPONSE_BODY = "Request body error. Please fix your request body";
     @FunctionName("TelegramWebhook")
     public HttpResponseMessage run(
             @HttpTrigger(
@@ -38,22 +39,26 @@ public class Function {
         final String body = request.getBody().orElse(null);
 
         if (body == null) {
-            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name in the request body").build();
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body(BAD_RESPONSE_BODY).build();
         }
 
         BotRequest botRequest = new BotRequestMapper(new ObjectMapper()).mapFrom(body);
 
+        if (botRequest.getMessage().getMessage_id() == 0 || botRequest.getMessage().getText() == null){
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body(BAD_RESPONSE_BODY).build();
+        }
+
         long messageId = botRequest.getMessage().getMessage_id();
 
-        String uri = "mongodb+srv://IndependentTeam:5UF2n0PnsIjaxYLN@cluster0.50iuewp.mongodb.net/?retryWrites=true&w=majority";
-
-        try (MongoClient mongoClient = MongoClients.create(uri)) {
-            MongoDatabase database = mongoClient.getDatabase("independentDB");
-            MongoCollection<Document> collection = database.getCollection("independentCollection");
-
-            Document doc = Document.parse(body);
-            collection.insertOne(doc);
-        }
+//        String uri = "mongodb+srv://IndependentTeam:5UF2n0PnsIjaxYLN@cluster0.50iuewp.mongodb.net/?retryWrites=true&w=majority";
+//
+//        try (MongoClient mongoClient = MongoClients.create(uri)) {
+//            MongoDatabase database = mongoClient.getDatabase("independentDB");
+//            MongoCollection<Document> collection = database.getCollection("independentCollection");
+//
+//            Document doc = Document.parse(body);
+//            collection.insertOne(doc);
+//        }
 
         return request.createResponseBuilder(HttpStatus.OK).body("message_id :  " + messageId).build();
 
--- kmaooad-functions/src/test/java/edu/kmaooad/FunctionTest.java
+++ kmaooad-functions/src/test/java/edu/kmaooad/FunctionTest.java
@@ -1,7 +1,6 @@
 package edu.kmaooad;
 
 import com.microsoft.azure.functions.*;
-import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 
 import java.util.*;
@@ -17,33 +16,78 @@ import static org.mockito.Mockito.*;
  * Unit test for Function class.
  */
 public class FunctionTest {
-    /**
-     * Unit test for HttpTriggerJava method.
-     */
-    @Test
-    public void testHttpTriggerJava() throws Exception {
-//        // Setup
-//        @SuppressWarnings("unchecked")
-//        final HttpRequestMessage<Optional<String>> req = mock(HttpRequestMessage.class);
-//
-//        final Optional<String> queryBody = Optional.of("Azure");
-//        doReturn(queryBody).when(req).getBody();
-//
-//        doAnswer(new Answer<HttpResponseMessage.Builder>() {
-//            @Override
-//            public HttpResponseMessage.Builder answer(InvocationOnMock invocation) {
-//                HttpStatus status = (HttpStatus) invocation.getArguments()[0];
-//                return new HttpResponseMessageMock.HttpResponseMessageBuilderMock().status(status);
-//            }
-//        }).when(req).createResponseBuilder(any(HttpStatus.class));
-//
-//        final ExecutionContext context = mock(ExecutionContext.class);
-//        doReturn(Logger.getGlobal()).when(context).getLogger();
-//
-//        // Invoke
-//        final HttpResponseMessage ret = new Function().run(req, context);
-//
-//        // Verify
-//        assertEquals(ret.getStatus(), HttpStatus.OK);
+
+    private static final String BAD_RESPONSE_BODY = "Request body error. Please fix your request body";
+
+    private HttpRequestMessage<Optional<String>> getReq(Optional<String> queryBody) {
+        // Setup
+        @SuppressWarnings("unchecked")
+        final HttpRequestMessage<Optional<String>> req = mock(HttpRequestMessage.class);
+
+        doReturn(queryBody).when(req).getBody();
+
+        doAnswer((Answer<HttpResponseMessage.Builder>) invocation -> {
+            HttpStatus status = (HttpStatus) invocation.getArguments()[0];
+            return new HttpResponseMessageMock.HttpResponseMessageBuilderMock().status(status);
+        }).when(req).createResponseBuilder(any(HttpStatus.class));
+
+        return req;
     }
+
+    private ExecutionContext getContext() {
+        // Setup
+        final ExecutionContext context = mock(ExecutionContext.class);
+        doReturn(Logger.getGlobal()).when(context).getLogger();
+        return context;
+    }
+
+    @Test
+    public void testFunctionWithoutBody() {
+        // Invoke
+        final HttpResponseMessage ret = new Function().run(getReq(Optional.empty()), getContext());
+        // Verify
+        assertEquals(HttpStatus.BAD_REQUEST, ret.getStatus());
+        assertEquals(BAD_RESPONSE_BODY, ret.getBody());
+    }
+
+    @Test
+    public void testFunctionWithMessageWithoutMessageIdInBody(){
+        // Invoke
+        final HttpResponseMessage ret = new Function().run(getReq(Optional.of("{\n" +
+                "    \"message\": {}\n" +
+                "}")), getContext());
+        // Verify
+        assertEquals(HttpStatus.BAD_REQUEST, ret.getStatus());
+        assertEquals(BAD_RESPONSE_BODY, ret.getBody());
+    }
+
+    @Test
+    public void testFunctionWithMessageIdWithoutMessageInBody(){
+        // Invoke
+        final HttpResponseMessage ret = new Function().run(getReq(Optional.of("{\n" +
+                "    \"message\": {\n" +
+                "        \"message_id\": 12345\n" +
+                "    }\n" +
+                "}")), getContext());
+        // Verify
+        assertEquals(HttpStatus.BAD_REQUEST, ret.getStatus());
+        assertEquals(BAD_RESPONSE_BODY, ret.getBody());
+    }
+
+
+    @Test
+    public void testFunctionWithCorrectBody(){
+        int ID = 12345;
+        // Invoke
+        final HttpResponseMessage ret = new Function().run(getReq(Optional.of("{\n" +
+                "    \"message\": {\n" +
+                "        \"message_id\": " + ID + ",\n" +
+                "        \"text\": \"TEST\"\n" +
+                "    }\n" +
+                "}")), getContext());
+        // Verify
+        assertEquals(HttpStatus.OK, ret.getStatus());
+        assertEquals("message_id :  " + ID, ret.getBody());
+    }
+
 }
