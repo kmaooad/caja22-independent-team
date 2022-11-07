d71e7770e8e41abecbc7fa131d1a32bd15fd0098
yostashko <yevhenii.ostashko@goto.com>
2022-11-08 01:37:08 +0300

    add config of bot

--- kmaooad-functions/src/main/java/edu/kmaooad/Application.java
+++ kmaooad-functions/src/main/java/edu/kmaooad/Application.java
@@ -1,12 +1,15 @@
 package edu.kmaooad;
 
 import org.springframework.boot.SpringApplication;
+import org.telegram.telegrambots.ApiContextInitializer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) throws Exception {
+        ApiContextInitializer.init();
+
         SpringApplication.run(Application.class, args);
     }
 }
